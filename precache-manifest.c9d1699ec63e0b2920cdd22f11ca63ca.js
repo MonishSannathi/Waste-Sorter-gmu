58cc9fa1010c873271d681f565677e186fc7274c
Monish Sannathi <sannathimonish@gmail.com>
2022-03-12 15:16:28 -0500

    Updates

---
+++ precache-manifest.c9d1699ec63e0b2920cdd22f11ca63ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0e7a56c5d2b24d10904f3fa278005ce",
+    "url": "/%7BWaste-Sorter-gmu%7D/index.html"
+  },
+  {
+    "revision": "a53dddf7f1b1fba3b629",
+    "url": "/%7BWaste-Sorter-gmu%7D/static/css/main.1e39348c.chunk.css"
+  },
+  {
+    "revision": "3d99d5bc13fe8ba27671",
+    "url": "/%7BWaste-Sorter-gmu%7D/static/js/2.52a24512.chunk.js"
+  },
+  {
+    "revision": "9e80ed6ee19e2ff5e581d429458bb084",
+    "url": "/%7BWaste-Sorter-gmu%7D/static/js/2.52a24512.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a53dddf7f1b1fba3b629",
+    "url": "/%7BWaste-Sorter-gmu%7D/static/js/main.64d0483b.chunk.js"
+  },
+  {
+    "revision": "d3b3a460a4d363e8b606",
+    "url": "/%7BWaste-Sorter-gmu%7D/static/js/runtime-main.ad36dbce.js"
+  }
+]);
